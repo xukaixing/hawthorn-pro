cfdcdd77c55bfbd5d36b3def21d24a0b05b2b67d
andy.ten <andy.ten@tom.com>
2021-04-05 20:48:21 +0800

    【提交类型】：added 【提交描述】：增加bus + rabbitmq方式刷新配置中心

--- hawthorn-starter-admin/src/main/java/com/hawthorn/admin/controller/HelloController.java
+++ hawthorn-starter-admin/src/main/java/com/hawthorn/admin/controller/HelloController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 @Slf4j
+@RefreshScope
 public class HelloController
 {
   @Autowired
